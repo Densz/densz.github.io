0c739adc3a0adc1b364262088b2247f6f03784af
Denis ZHENG <zheng.denis@gmail.com>
2019-02-27 17:38:24 +0100

    Mouse animation test

---
+++ precache-manifest.cd359133bf3e28b4944ed08b3e1e144f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "015f5bffcd68eb67464c37093ebc5015",
+    "url": "/static/media/profile.015f5bff.jpg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "e1d0c7a6c368aa953193",
+    "url": "/static/js/main.e1d0c7a6.chunk.js"
+  },
+  {
+    "revision": "c92cecf17e22561c6198",
+    "url": "/static/js/2.c92cecf1.chunk.js"
+  },
+  {
+    "revision": "e1d0c7a6c368aa953193",
+    "url": "/static/css/main.982f8748.chunk.css"
+  },
+  {
+    "revision": "a09f7823fca8f3ef13ce7cd6054500b0",
+    "url": "/index.html"
+  }
+];
